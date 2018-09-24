80481027dbf95b86607244e3c8bb522143a8f61a
Henrik Nilsson <henrik@autopi.io>
2018-09-24 15:30:24 +0200

    Addded 'wifi.status' command

--- src/salt/base/ext/_modules/wifi.py
+++ src/salt/base/ext/_modules/wifi.py
@@ -1,4 +1,5 @@
 import logging
+import parsing
 
 from iw_parse import get_interfaces
 
@@ -19,6 +20,17 @@ def help():
     """
 
     return __salt__["sys.doc"](__virtualname__)
+
+
+def status(interface="wlan0"):
+	"""
+	Get current WPA/EAPOL/EAP status.
+	"""
+
+	res = __salt__["cmd.run"]("wpa_cli -i {:} status".format(interface))
+    ret = parsing.into_dict_parser(res, separator="=")
+
+    return ret
 
 
 def scan(interface="wlan0"):
