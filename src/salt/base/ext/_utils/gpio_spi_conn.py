82061c75cde1cc4943a2fd39bba8624cfef30384
root <root@cloud01-qa.autopi.io>
2018-11-09 12:24:35 +0000

    Fix: Ensure that we have a sufficient delay between send and receive when communicating with SPM

--- src/salt/base/ext/_utils/gpio_spi_conn.py
+++ src/salt/base/ext/_utils/gpio_spi_conn.py
@@ -3,6 +3,7 @@ import RPi.GPIO as gpio
 import time
 
 from retrying import retry
+from timeit import default_timer as timer
 
 
 log = logging.getLogger(__name__)
@@ -10,12 +11,15 @@ log = logging.getLogger(__name__)
 
 class GPIOSPIConn(object):
 
-    def __init__(self, clk_pin, mosi_pin, miso_pin, num_bits=8, gpio_mode=gpio.BOARD):
+    def __init__(self, clk_pin, mosi_pin, miso_pin, num_bits=8, gpio_mode=gpio.BOARD, send_recv_delay=0.0005):
         self._clk_pin = clk_pin
         self._mosi_pin = mosi_pin
         self._miso_pin = miso_pin
         self._num_bits = num_bits
         self._gpio_mode = gpio_mode
+
+        self.send_recv_delay = send_recv_delay
+        self.last_send = 0
 
     def setup(self):
         log.info("Setting up GPIO SPI connection")
@@ -42,7 +46,8 @@ class GPIOSPIConn(object):
         Sends 1 byte of data or less.
         """
 
-        log.debug("TX: {:}".format(data))
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("TX: {:}".format(data))
 
         data <<= (8 - self._num_bits)
 
@@ -60,6 +65,8 @@ class GPIOSPIConn(object):
             gpio.output(self._clk_pin, gpio.HIGH)
             gpio.output(self._clk_pin, gpio.LOW)
 
+        self.last_send = timer()
+
     @retry(stop_max_attempt_number=3, wait_fixed=500)
     def recv(self, ack=None):
         """
@@ -67,6 +74,13 @@ class GPIOSPIConn(object):
         """
 
         ret = 0
+
+        # Ensure that we have a sufficient delay between send and receive (needed because RPi3 is too fast)
+        delay = 0
+        split_time = timer() - self.last_send
+        if split_time < self.send_recv_delay:
+            delay = self.send_recv_delay - split_time
+            time.sleep(delay)
 
         for bit in range(self._num_bits):
             # Pulse clock pin
@@ -83,7 +97,11 @@ class GPIOSPIConn(object):
         # Divide by two to drop the NULL bit
         ret = ret/2
 
-        log.debug("RX: {:}".format(ret))
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("RX: {:}".format(ret))
+
+            if delay > 0:
+                log.debug("Delay of {:} second was added between last send and current receive".format(delay))
 
         # TODO: We always need to receive an ack first to see if we got what we expected?
         if ack != None and ack != ret:
