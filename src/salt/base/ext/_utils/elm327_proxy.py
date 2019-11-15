25685ff28c98e7d9106448b60b6c82d021e309d9
Henrik Nilsson <henrik@autopi.io>
2019-11-15 18:07:30 +0100

    Ensure only one client thread in ELM327 proxy

--- src/salt/base/ext/_utils/elm327_proxy.py
+++ src/salt/base/ext/_utils/elm327_proxy.py
@@ -1,4 +1,3 @@
-import errno
 import logging
 import socket
 import thread
@@ -18,11 +17,13 @@ class ELM327Proxy(threading.Thread):
         super(ELM327Proxy, self).__init__()
 
         self.name = "elm327_proxy_{:}".format(id(self))
-        self.daemon = True
+        self.daemon = False
 
         self.on_command = None
         self.on_connect = None
         self.on_disconnect = None
+
+        self.multiple_clients = False
 
         self._sock = None
         self._host = None
@@ -34,7 +35,7 @@ class ELM327Proxy(threading.Thread):
 
         return super(ELM327Proxy, self).start()
 
-    @retry(wait_fixed=5000)
+    @retry(wait_fixed=10000)
     def run(self):
         try:
             if log.isEnabledFor(logging.DEBUG):
@@ -42,18 +43,25 @@ class ELM327Proxy(threading.Thread):
 
             self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._sock.bind((self._host, self._port))
-            self._sock.listen(1)  # Only allow one connection
-
-            log.info("ELM327 proxy listening on %s:%s", self._host, self._port)
+            self._sock.listen(0)
 
             while True:
+                log.info("ELM327 proxy is ready and listening on %s:%s", self._host, self._port)
 
                 # Wait to accept a connection - blocking call
                 conn, addr = self._sock.accept()
                 log.info("Accepted new connection from ELM327 client %s:%s", addr[0], addr[1])
 
-                # Spawn new dedicated client thread
-                thread.start_new_thread(self._client_thread, (conn, addr))
+                if self.multiple_clients:
+
+                    # Spawn new dedicated client thread
+                    thread.start_new_thread(self._client_thread, (conn, addr))
+
+                else:
+
+                    # Run in current thread
+                    self._client_thread(conn, addr)
+
         except:
             log.exception("Unhandled error in ELM327 proxy")
 
@@ -61,6 +69,7 @@ class ELM327Proxy(threading.Thread):
         finally:
             log.info("Stopping ELM327 proxy")
 
+            self._sock.shutdown(socket.SHUT_RDWR)
             self._sock.close()
 
     def _client_thread(self, conn, addr):
@@ -73,11 +82,28 @@ class ELM327Proxy(threading.Thread):
                 except:
                     log.exception("Error in 'on_connect' event handler")
 
+            #if log.isEnabledFor(logging.DEBUG):
+            log.info("Sending initial ready prompt")
+
             # Send initial ready prompt
             conn.send(self.PROMPT)
 
             while True:
-                for cmd in self._read(conn).rstrip().split(self.TERMINATOR):
+                data = self._read(conn)
+                if not data:
+
+                    #if log.isEnabledFor(logging.DEBUG):
+                    log.info("No data received - sending ready prompt")
+
+                    # Send ready prompt again
+                    conn.send(self.PROMPT)
+
+                    continue
+
+                if log.isEnabledFor(logging.DEBUG):
+                    log.debug("RX: %s", repr(data))
+
+                for cmd in data.rstrip().split(self.TERMINATOR):
                     res = self.TERMINATOR.join(self.on_command(cmd)) + self.PROMPT
 
                     if log.isEnabledFor(logging.DEBUG):
@@ -85,10 +111,8 @@ class ELM327Proxy(threading.Thread):
 
                     conn.send(res)
         except socket.error as err:
-            if err.errno == errno.EPIPE:
-                log.info("Broken pipe in ELM327 client connection %s:%s", addr[0], addr[1])
-            else:
-                log.exception("Socket error in ELM327 client connection %s:%s", addr[0], addr[1])
+            log.info("Disconnected ELM327 client connection %s:%s due to socket error: %s", addr[0], addr[1], str(err))
+
         except:
             log.exception("Unhandled error in ELM327 client connection %s:%s", addr[0], addr[1])
 
@@ -109,16 +133,12 @@ class ELM327Proxy(threading.Thread):
         while True:
             data = conn.recv(1024)
             if not data:
-                log.warning("No data received")
-
                 break
 
             buffer.extend(data)
 
+            # Break on CR or NL
             if buffer[-1:] in [b"\r", b"\n"]:
                 break
-
-        if log.isEnabledFor(logging.DEBUG):
-            log.debug("RX: %s", repr(buffer.decode()))
 
         return buffer.decode()
