678870f2f51ac613b7b1a0f04bb475af99cf3c9b
Henrik Nilsson <henrik@autopi.io>
2020-01-08 09:27:42 +0100

    Refactored 'qmi.*' commands

--- src/salt/base/ext/_modules/qmi.py
+++ src/salt/base/ext/_modules/qmi.py
@@ -1,14 +1,8 @@
 import logging
-import parsing
-
 import qmilib
 
 
 log = logging.getLogger(__name__)
-
-
-def _run(name, device="/dev/cdc-wdm0"):
-    return __salt__["cmd.run"]("qmicli --silent -d {:s} --{:s}".format(device, name))
 
 
 def help():
@@ -19,16 +13,47 @@ def help():
     return __salt__["sys.doc"]("qmi")
 
 
+def cell_info():
+    """
+    Get cell location info.
+    """
+
+    return qmilib.nas_get_cell_location_info()
+
+
+def connection_status():
+    """
+    Get packet service status.
+    """
+
+    return qmilib.wds_get_packet_service_status()
+
+
+def connection_stats():
+    """
+    Get packet statistics.
+    """
+
+    return qmilib.wds_get_packet_statistics()
+
+
 def operator_name():
     """
     Get operator name data.
     """
 
-    # TODO: Complete this
-    out = _run("nas-get-operator-name")
-    #ret = parsing.into_dict_parser(out)
+    return qmilib.nas_get_operator_name()
 
-    return out
+
+def signal_strength(rated_only=False):
+    """
+    Get current signal strength values.
+
+    Optional arguments:
+      - rated_only (bool): Default is 'False'.
+    """
+
+    return qmilib.nas_get_signal_strength(rated_only=rated_only)
 
 
 def system_info():
@@ -36,53 +61,4 @@ def system_info():
     Get system info.
     """
 
-    # TODO: Complete this
-    out = _run("nas-get-system-info")
-    #ret = parsing.into_dict_parser(out)
-
-    return out
-
-
-def cell_info():
-    """
-    Get cell location info.
-    """
-
-    # TODO: Complete this
-    out = _run("nas-get-cell-location-info")
-    #ret = parsing.into_dict_parser(out)
-
-    return out
-
-
-def signal_strength(rated_only=False):
-    """
-    Current signal strength values.
-
-    Optional arguments:
-      - rated_only (bool): Default is 'False'.
-    """
-
-    out = _run("nas-get-signal-strength")
-    ret = qmilib.parse_signal_strength(out, skip_unrated=rated_only)
-
-    return ret
-
-
-# TODO: Remove this command becasue above is better?
-def nas_get_signal_info():
-    """
-    """
-
-    out = _run("nas-get-signal-info")
-    lines = parsing.lines_parser(out)
-
-    ret = {}
-
-    if not re.search("success", lines[0], re.IGNORECASE):
-        ret["error"] = lines[0]
-        return ret
-
-    parsing.into_dict_parser(lines[1:], root=ret, value_parser=parsing.number_parser)
-
-    return ret
+    return qmilib.nas_get_system_info()
--- src/salt/base/state/network/wwan/qmi/pylib/__init__.py
+++ src/salt/base/state/network/wwan/qmi/pylib/__init__.py
@@ -1,67 +1,121 @@
-from .parser import *
+import subprocess
+
+from .parser import parse_signal_strength
 
 
+def cli(option, device="/dev/cdc-wdm0"):
+    """
+    Call QMI command line interface.
 
-#Mode preference: gsm, umts, lte
-#Service preference: wcdma
+    Arguments:
+      - option (str): Command option.
 
-def network_summary():
-    ret = {
-        "operator": "",
-        "services": {
-            "available": [],
-            "selected": ""
-        }
-    }
+    Optional arguments:
+      - device (str): Default is '/dev/cdc-wdm0'.
+    """
+
+    p = subprocess.Popen(["qmicli", "--silent", "--device={:s}".format(device), "--{:s}".format(name)],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = p.stdout.read()
+    err = p.stderr.read()
+
+    if err:
+        raise Exception(err)
+
+    return out
+
 
 #
 # Network Access Service
 #
 
-def network_operator_name():
-    pass
-
-    #qmicli --device-open-$MODE --device $DEVICE --nas-get-operator-name
-
-def network_system_info():
-    pass
-
-    # --nas-get-system-info
-
-def network_services():
-    pass
-
-    res = system_info()
-    #GSM service:\nStatus: 'available'
-    #WCDMA service:\nStatus: 'available'
-    #LTE service:\nStatus: 'available'
-
-def network_serving_system():
-    pass
-
-    # Get selected network
-    #--nas-get-serving-system
-    # Selected network: '3gpp'
-
-def network_signal(rated_only=False):
+def nas_get_cell_location_info():
     """
-    Current signal strength values.
+    Get cell location info.
     """
 
-    out = _run("nas-get-signal-strength")
-    ret = parse_signal_strength(out, skip_unrated=rated_only)
+    out = cli("nas-get-cell-location-info")
+
+    return out
+
+def nas_get_operator_name():
+    """
+    Get operator name data.
+    """
+
+    out = cli("nas-get-operator-name")
+
+    return out
+
+def nas_get_system_info():
+    """
+    Get system info.
+    """
+
+    out = cli("nas-get-system-info")
+
+    return out
+
+def nas_get_serving_system():
+    """
+    Get serving system.
+    """
+
+    out = cli("nas-get-serving-system")
+
+    return out
+
+def nas_get_signal_info():
+    """
+    Get signal strength.
+    """
+
+    out = cli("nas-get-signal-info")
+
+    """
+    lines = parsing.lines_parser(out)
+
+    ret = {}
+
+    parsing.into_dict_parser(lines[1:], root=ret, value_parser=parsing.number_parser)
 
     return ret
+    """
+
+    return out
+
+def nas_get_signal_strength(rated_only=False):
+    """
+    Get signal strength.
+
+    Optional arguments:
+      - rated_only (bool): Default is 'False'.
+    """
+
+    out = cli("nas-get-signal-strength")
+
+    return parse_signal_strength(out, skip_unrated=rated_only)
 
 
 #
 # Wireless Data Service
 #
 
-def connection_status():
-    pass
-    #--wds-get-packet-service-status
+def wds_get_packet_service_status():
+    """
+    Get packet service status.
+    """
 
-def connection_stats():
-    pass
-    #--wds-get-packet-statistics
+    out = cli("wds-get-packet-service-status")
+
+    return out
+
+
+def wds_get_packet_statistics():
+    """
+    Get packet statistics.
+    """
+
+    out = cli("wds-get-packet-statistics")
+
+    return out
