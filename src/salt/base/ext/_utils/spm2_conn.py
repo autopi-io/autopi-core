8b2ef5538a85a5aef6df0b12e2f1ecdfc597cbd5
Henrik Nilsson <henrik@autopi.io>
2020-04-15 10:53:35 +0200

    Work on SPM2 connection

--- src/salt/base/ext/_utils/spm2_conn.py
+++ src/salt/base/ext/_utils/spm2_conn.py
@@ -6,14 +6,23 @@ from retrying import retry
 
 
 # Registers
-HEARTBEAT       = 0x00
-STATUS          = 0x01
-STATS           = 0x02
-TIMER           = 0x03
-VOLTAGE         = 0x04
-VERSION         = 0x05
-PINS            = 0x0A
-SLEEP_DURATION  = 0x0B
+REG_HEARTBEAT       = 0x00
+REG_STATUS          = 0x01
+REG_STATS           = 0x02
+REG_TIMER           = 0x03
+REG_VOLTAGE         = 0x04
+REG_VERSION         = 0x05
+REG_WAKE_FLAGS      = 0x06
+REG_PINS            = 0x0A
+REG_SLEEP_DURATION  = 0x0B
+
+# Wake flags
+WAKE_FLAG_UNKNOWN  = (1 << 0x00)
+WAKE_FLAG_STN      = (1 << 0x01)
+WAKE_FLAG_RTC      = (1 << 0x02)
+WAKE_FLAG_ACC      = (1 << 0x03)
+WAKE_FLAG_GYRO     = (1 << 0x04)
+WAKE_FLAG_MODEM    = (1 << 0x05)
 
 # Pins
 PIN_IN_RPI_PWR       = (1 << 0x00)
@@ -24,20 +33,6 @@ PIN_OUT_RPI_SHUTDN   = (1 << 0x04)
 PIN_OUT_STN_SLEEP    = (1 << 0x05)
 PIN_OUT_STN_RESET    = (1 << 0x06)
 PIN_OUT_MODEM_DTR    = (1 << 0x07)
-
-INPUT_PINS = {
-    "rpi_pwr":       PIN_IN_RPI_PWR,
-    "stn_pwr_ctrl":  PIN_IN_STN_PWR_CTRL
-}
-
-OUTPUT_PINS = {
-    "sw_5v":       PIN_OUT_SW_5V,
-    "sw_3v3":      PIN_OUT_SW_3V3,
-    "rpi_shutdn":  PIN_OUT_RPI_SHUTDN,
-    "stn_sleep":   PIN_OUT_STN_SLEEP,
-    "stn_reset":   PIN_OUT_STN_RESET,
-    "modem_dtr":   PIN_OUT_MODEM_DTR
-}
 
 # Status register
 STATES = {
@@ -61,7 +56,30 @@ TRIGGERS = {
     0x08: "timer",
     0x09: "boot_timeout",
     0x0A: "heartbeat_timeout",
-    0xFF: "unknown"
+    0xFE: "unknown"
+}
+
+WAKE_FLAGS = {
+    "unknown": WAKE_FLAG_UNKNOWN,
+    "stn": WAKE_FLAG_STN,
+    "rtc": WAKE_FLAG_RTC,
+    "acc": WAKE_FLAG_ACC,
+    "gyro": WAKE_FLAG_GYRO,
+    "modem": WAKE_FLAG_MODEM
+}
+
+INPUT_PINS = {
+    "rpi_pwr":       PIN_IN_RPI_PWR,
+    "stn_pwr_ctrl":  PIN_IN_STN_PWR_CTRL
+}
+
+OUTPUT_PINS = {
+    "sw_5v":       PIN_OUT_SW_5V,
+    "sw_3v3":      PIN_OUT_SW_3V3,
+    "rpi_shutdn":  PIN_OUT_RPI_SHUTDN,
+    "stn_sleep":   PIN_OUT_STN_SLEEP,
+    "stn_reset":   PIN_OUT_STN_RESET,
+    "modem_dtr":   PIN_OUT_MODEM_DTR
 }
 
 
@@ -70,13 +88,30 @@ log = logging.getLogger(__name__)
 
 class SPM2Conn(I2CConn):
 
+    def read_block(self, register, length):
+        res = super(SPM2Conn, self).read_block(register, length)
+
+        # Validate response
+        if not res:
+            log.warn("Empty response received when reading register {:}".format(register))
+
+            raise Exception("Empty response received when reading register {:}".format(register))
+
+        if res == [r for r in res if r == 0xFF]:  # Are all bytes 0xFF?
+            log.warn("Received seemingly invalid response when reading register {:}: {:}".format(register, res))
+
+            raise Exception("Received seemingly invalid response when reading register {:}: {:}".format(register, res))
+
+        return res
+
+
     @retry(stop_max_attempt_number=3, wait_fixed=200)
     def heartbeat(self):
         """
         Send heartbeat.
         """
 
-        res = self.read_block(HEARTBEAT, 1)
+        res = self.read_block(REG_HEARTBEAT, 1)
 
         return res
 
@@ -86,7 +121,7 @@ class SPM2Conn(I2CConn):
         Get current status.
         """
 
-        res = self.read_block(STATUS, 5)
+        res = self.read_block(REG_STATUS, 5)
 
         ret = {
             "current_state": STATES.get(res[0], "invalid"),
@@ -108,7 +143,7 @@ class SPM2Conn(I2CConn):
         Get life cycle statistics.
         """
 
-        res = self.read_block(STATS, 9)
+        res = self.read_block(REG_STATS, 9)
 
         ret = {
             "completed_cycles": (res[1] << 8) + (res[0] << 0),
@@ -127,9 +162,21 @@ class SPM2Conn(I2CConn):
         Get current timer value in milliseconds.
         """
 
-        res = self.read_block(TIMER, 4)
+        res = self.read_block(REG_TIMER, 4)
 
         return (res[3] << 24) + (res[2] << 16) + (res[1] << 8) + (res[0] << 0)
+
+    @retry(stop_max_attempt_number=3, wait_fixed=200)
+    def wake_flags(self):
+        """
+        Get current wake event flags. 
+        """
+
+        res = self.read_block(REG_WAKE_FLAGS, 1)
+
+        ret = {k: bool(res[0] & v) for k, v in WAKE_FLAGS.iteritems()}
+
+        return ret
 
     @retry(stop_max_attempt_number=3, wait_fixed=200)
     def pins(self, toggle=None):
@@ -145,9 +192,9 @@ class SPM2Conn(I2CConn):
 
                 val |= OUTPUT_PINS[pin]
 
-            self.write_block(PINS, [val])
+            self.write_block(REG_PINS, [val])
 
-        res = self.read_block(PINS, 1)
+        res = self.read_block(REG_PINS, 1)
 
         ret = {
             "input": {k: bool(res[0] & v) for k, v in INPUT_PINS.iteritems()},
@@ -164,7 +211,7 @@ class SPM2Conn(I2CConn):
 
         raise NotImplementedError()
 
-        res = self.read_block(VOLTAGE, 2)
+        res = self.read_block(REG_VOLTAGE, 2)
 
         return res
 
@@ -174,7 +221,7 @@ class SPM2Conn(I2CConn):
         Get firmware version.
         """
 
-        res = self.read_block(VERSION, 2)
+        res = self.read_block(REG_VERSION, 2)
 
         return "{:d}.{:d}".format(*res)
 
@@ -185,9 +232,9 @@ class SPM2Conn(I2CConn):
         """
 
         if value != None:
-            self.write_block(SLEEP_DURATION, [(value >> i * 8) & 0xFF for i in range(0, 4)])
+            self.write_block(REG_SLEEP_DURATION, [(value >> i * 8) & 0xFF for i in range(0, 4)])
         
-        res = self.read_block(SLEEP_DURATION, 4)
+        res = self.read_block(REG_SLEEP_DURATION, 4)
 
         return (res[3] << 24) + (res[2] << 16) + (res[1] << 8) + (res[0] << 0)
 
