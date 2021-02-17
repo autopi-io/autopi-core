beeca3c9254ac8be0946b4b91d5c800c46bff272
Henrik Nilsson <henrik@autopi.io>
2021-02-17 11:29:10 +0100

    Fixed config attrs of JSONL returner

--- src/salt/base/ext/_returners/cloud_jsonl_returner.py
+++ src/salt/base/ext/_returners/cloud_jsonl_returner.py
@@ -53,25 +53,26 @@ def _get_options(ret):
         "filename": "{now:%y%m%d%H%M%S}_{pid}.jsonl",
         "dir": "/opt/autopi/data",
         "mode": "a",
-        "async_writer": {
-            "flush_threshold": int(1024*1024*0.5),  # 0.5MB
-            "flush_timeout": 1,                     # 1 second
-            "buffer_size": 1024*1024*10,            # 10MB
-            "buffer_high_watermark": 0
-        },
-        "rotation": {
-            "size": 1024*1024*100,  # 100MB
-            "cron": "0 0 * * *",    # At 00:00 every day
-            "interval": 0           # Disabled
-        }
+        "async_writer.flush_threshold": int(1024*1024*0.5),  # 0.5MB
+        "async_writer.flush_timeout": 1,                     # 1 second
+        "async_writer.buffer_size": 1024*1024*10,            # 10MB
+        "async_writer.buffer_high_watermark": 0,
+        "rotation.size": 1024*1024*100,                      # 100MB
+        "rotation.cron": "0 0 * * *",                        # At 00:00 every day
+        "rotation.interval": 0                               # Disabled
     }
 
     attrs = {
         "filename": "filename",
         "dir": "dir",
         "mode": "mode",
-        "async_writer": "async_writer",
-        "rotation": "rotation"
+        "async_writer.flush_threshold": "async_writer.flush_threshold",
+        "async_writer.flush_timeout": "async_writer.flush_timeout",
+        "async_writer.buffer_size": "async_writer.buffer_size",
+        "async_writer.buffer_high_watermark": "async_writer.buffer_high_watermark",
+        "rotation.size": "rotation.size",
+        "rotation.cron": "rotation.cron",
+        "rotation.interval": "rotation.interval"
     }
 
     _options = salt.returners.get_returner_options(
@@ -113,20 +114,21 @@ def _get_writer_for(ret):
                     filename = options["filename"].format(now=now, pid=os.getpid(), tid=threading.currentThread().ident)
 
                     # Set max file size
-                    obj.max_size = options["rotation"].get("size", 0)
+                    obj.max_size = options["rotation.size"]
 
                     # Calculate and set next expiration time
                     expiration_times = []
-                    if options["rotation"].get("interval", 0) > 0:
-                        expiration_times.append(float((now + datetime.timedelta(seconds=options["rotation"]["interval"])).strftime("%s")))
-                    if options["rotation"].get("cron", None):
-                        expiration_times.append(float(croniter.croniter(options["rotation"]["cron"], now).get_next()))
+                    if options["rotation.interval"] > 0:
+                        expiration_times.append(float((now + datetime.timedelta(seconds=options["rotation.interval"])).strftime("%s")))
+                    if options["rotation.cron"]:
+                        expiration_times.append(float(croniter.croniter(options["rotation.cron"], now).get_next()))
                     obj.expiration_time = min(expiration_times) if expiration_times else 0
 
                     log.info("Opening JSONL file '{:}'".format(filename))
                     return open(os.path.join(options["dir"], filename), "a", buffering=0)  # Buffering disabled
 
-                writer = AsyncWriterThread(RotatingTextFile(open_file), **options["async_writer"])
+                writer = AsyncWriterThread(RotatingTextFile(open_file),
+                    **{k[len("async_writer."):]: v for k, v in options.iteritems() if k.startswith("async_writer.")})
                 
                 # Store writer in context
                 __context__["cloud_jsonl_writers"][key] = writer
