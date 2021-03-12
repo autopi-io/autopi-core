1372d7be24be7e10a5ac0d4e417f4aba42d9a4cf
Nikola Velichkov <nikola@autopi.io>
2021-03-12 10:45:43 +0100

    Skip communication_event_trigger if result is a warning

--- src/salt/base/ext/_engines/obd_manager.py
+++ src/salt/base/ext/_engines/obd_manager.py
@@ -1295,6 +1295,9 @@ def communication_event_trigger(result):
     if error and log.isEnabledFor(logging.DEBUG):
         log.debug("Communication event trigger got error result: {:}".format(result))
 
+    if error and isinstance(error, Warning):
+        log.info("Communication event trigger got a warning, skipping.")
+
     # Check if state has chaged since last known state
     ctx = context.setdefault("communication", {})
     old_state = ctx.get("state", "")
