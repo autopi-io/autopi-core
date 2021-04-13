3f150cfa6fb443c26c2022c585aa49de9f55e904
Henrik Nilsson <henrik@autopi.io>
2021-04-13 15:44:22 +0200

    Added expiration functionality to 'cloud_jsonl_returner'

--- src/salt/base/ext/_returners/cloud_jsonl_returner.py
+++ src/salt/base/ext/_returners/cloud_jsonl_returner.py
@@ -14,6 +14,7 @@ import salt.utils
 from cloud_cache import prepare_result_recursively
 from common_util import jsonl_dump, makedirs, RotatingTextFile
 from threading_more import AsyncWriterThread, on_exit
+from timeit import default_timer as timer
 
 
 JSON_SEPARATORS = (",", ":")
@@ -35,7 +36,7 @@ def __virtual__():
 
 
 def _exit_handler():
-    for filename, writer in __context__.get("cloud_jsonl_writers", {}).iteritems():
+    for filename, writer in __context__.get("cloud_jsonl_returner.writers", {}).iteritems():
         try:
             writer.stop()
         except:
@@ -52,6 +53,7 @@ def _get_options(ret):
         "filename": "{now:%y%m%d%H%M%S}_{pid}.jsonl",
         "dir": "/opt/autopi/data",
         "mode": "a",
+        "initially_expired": False,
         "async_writer.flush_threshold": int(1024*1024*0.5),  # 0.5MB
         "async_writer.flush_timeout": 1,                     # 1 second
         "async_writer.buffer_size": 1024*1024*10,            # 10MB
@@ -65,6 +67,7 @@ def _get_options(ret):
         "filename": "filename",
         "dir": "dir",
         "mode": "mode",
+        "initially_expired": "initially_expired",
         "async_writer.flush_threshold": "async_writer.flush_threshold",
         "async_writer.flush_timeout": "async_writer.flush_timeout",
         "async_writer.buffer_size": "async_writer.buffer_size",
@@ -80,7 +83,7 @@ def _get_options(ret):
         attrs,
         __salt__=__salt__,
         __opts__=__opts__,
-        defaults=defaults,
+        defaults=defaults
     )
 
     if DEBUG:
@@ -99,18 +102,20 @@ def _get_writer_for(ret):
         with tlock:
             options = _get_options(ret)
 
-            # Ensure directory exists
-            makedirs(options["dir"], exist_ok=True)
-
-            key = options["filename"].format(now=EPOC_DATETIME, pid=os.getpid(), tid=threading.currentThread().ident)
-            writer = __context__.setdefault("cloud_jsonl_writers", {}).get(key, None)
+            key = options["filename"].format(now=EPOC_DATETIME, uid=__opts__["id"], pid=os.getpid(), tid=threading.currentThread().ident)
+            writer = __context__.setdefault("cloud_jsonl_returner.writers", {}).get(key, None)
             if not writer or writer.closed:
 
                 def open_file(obj):
                     now = datetime.datetime.utcnow()
                     
+                    format_kwargs = dict(now=now, uid=__opts__["id"], pid=os.getpid(), tid=threading.currentThread().ident)
+
+                    # Generate directory
+                    directory = options["dir"].format(**format_kwargs)
+
                     # Generate filename
-                    filename = options["filename"].format(now=now, pid=os.getpid(), tid=threading.currentThread().ident)
+                    filename = options["filename"].format(**format_kwargs)
 
                     # Set max file size
                     obj.max_size = options["rotation.size"]
@@ -123,14 +128,17 @@ def _get_writer_for(ret):
                         expiration_times.append(float(croniter.croniter(options["rotation.cron"], now).get_next()))
                     obj.expiration_time = min(expiration_times) if expiration_times else 0
 
+                    # Ensure directory exists
+                    makedirs(directory, exist_ok=True)
+
                     log.info("Opening JSONL file '{:}'".format(filename))
-                    return open(os.path.join(options["dir"], filename), "a", buffering=0)  # Buffering disabled
+                    return open(os.path.join(directory, filename), "a", buffering=0)  # Buffering disabled
 
                 writer = AsyncWriterThread(RotatingTextFile(open_file),
                     **{k[len("async_writer."):]: v for k, v in options.iteritems() if k.startswith("async_writer.")})
-                
+
                 # Store writer in context
-                __context__["cloud_jsonl_writers"][key] = writer
+                __context__["cloud_jsonl_returner.writers"][key] = writer
 
             elif DEBUG:
                 log.debug("Re-using writer found in context")
@@ -142,6 +150,78 @@ def _get_writer_for(ret):
         log.debug("Re-using writer found in thread local")
 
     return writer
+
+
+def _is_expired_for(ret):
+
+    # Initialize if not already done
+    if not "cloud_jsonl_returner.expiration" in __context__:
+        options = _get_options(ret)
+
+        delay = -1  # Disabled
+        if options["initially_expired"]:
+            delay = 0  # Instantly
+
+            log.info("Returner is initially expired")
+
+        set_expiration(delay, reason="initial")
+
+    return is_expired()
+
+
+def is_expired():
+    """
+    Check if this returner has expired.
+    """
+
+    time = __context__.get("cloud_jsonl_returner.expiration", {}).get("time", -1)
+    if time < 0:
+        return False
+
+    if time > 0:
+        return time <= timer()
+    else:
+        return True
+
+
+def set_expiration(delay, reason="unknown"):
+    """
+    Set or clear expiration for this returner.
+    Delay of zero means instant expiration.
+    Negative delay will clear any already set expiration.
+    """
+
+    ctx = __context__.setdefault("cloud_jsonl_returner.expiration", {})
+    if delay < 0:
+        if ctx:
+            ctx.clear()
+
+            log.info("Cleared expiration with reason '{:}'".format(reason))
+    else:
+        if is_expired():
+            log.info("Already expired with reason '{:}'".format(ctx.get("reason", "")))
+        else:
+            if delay > 0:
+                ctx["time"] = float((datetime.datetime.now() + datetime.timedelta(seconds=delay)).strftime("%s"))
+            else:
+                ctx["time"] = 0  # Instantly
+            ctx["reason"] = reason
+
+            log.info("Set expiration to {:} with reason '{:}'".format(ctx["time"], ctx["reason"]))
+
+    return ctx
+
+
+def close_writer():
+    """
+    Ensure any open writer is closed for the calling thread.
+    """
+
+    writer = getattr(tlocal, "writer", None)
+    if writer and not writer.closed:
+        log.info("Closing writer found in thread local: {:}".format(writer))
+
+        writer.close()
 
 
 def returner(ret):
@@ -166,6 +246,14 @@ def returner_job(job):
 
     if not ret or not job.get("success", True):  # Default is success if not specified
         log.warn("Skipping unsuccessful job result with JID {:}: {:}".format(job["jid"], job))
+
+        return
+
+    if _is_expired_for(job):
+        if DEBUG:
+            log.debug("Skipping job result because the returner has expired")
+
+        close_writer()
 
         return
 
@@ -210,6 +298,14 @@ def returner_data(data, kind, **kwargs):
 
     if "error" in data:
         log.warn("Skipping data result because of error: {:}".format(data["error"]))
+
+        return
+
+    if _is_expired_for(kwargs):
+        if DEBUG:
+            log.debug("Skipping data result because the returner has expired")
+
+        close_writer()
 
         return
 
