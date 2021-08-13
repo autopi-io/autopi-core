7b36cd76809ed5f105de94d9fb7e085d1e1b429d
Malte Baden Hansen <malte@maltebaden.com>
2021-08-13 16:49:10 +0200

    Changed docker_monitor to not use while loop as it will be restarted automatically anyway.

--- src/salt/base/ext/_engines/docker_monitor.py
+++ src/salt/base/ext/_engines/docker_monitor.py
@@ -22,45 +22,38 @@ def start(**settings):
         log.info("Starting docker_monitor with settings: {:}".format(settings))
 
         # If balena-engine restarts it will break the for loop, so keep it in a while loop.
-        while True:
-            log.info('Creating dockerclient and Attaching to docker.events')
-            docker_path = __salt__["config.get"]("docker.url", 'unix://var/run/balena-engine.sock')
-            client = docker.DockerClient(base_url=docker_path)
-            
-            since = context["newest_event_timestamp"]
-            if not since:
-                # Ensure that older events not caught when they occurred is still processed, else just proceed from "NOW"/Last event.
-                since = int(time()) - 120
+        log.info('Creating dockerclient and Attaching to docker.events')
+        docker_path = __salt__["config.get"]("docker.url", 'unix://var/run/balena-engine.sock')
+        client = docker.DockerClient(base_url=docker_path)
+        
+        since = context["newest_event_timestamp"]
+        past_events_seconds = 120
+        if not since:
+            # Ensure that older events not caught when they occurred is still processed, else just proceed from "NOW"/Last event.
+            since = int(time()) - past_events_seconds
 
-            for event in client.events(decode=True, since=since, filters={ "type": "container", "event": ["start", "stop"] }):
-                # If event is newer than newest_event_timestamp, trigger events.
-                if not context["newest_event_timestamp"] or event["time"] > context["newest_event_timestamp"]:
-                    context["newest_event_timestamp"] = event["time"]
+        for event in client.events(decode=True, since=since, filters={ "type": "container", "event": ["start", "stop"] }):
+            # If event is newer than newest_event_timestamp, trigger events.
+            if not context["newest_event_timestamp"] or event["time"] > context["newest_event_timestamp"]:
+                context["newest_event_timestamp"] = event["time"]
 
-                    try:
-                        event_string = "system/docker/container/{:}/{:}".format(event["Actor"]["Attributes"]["name"], event["Action"])
-                        log.info("Triggering event: {}".format(event))
-                        __salt__["minionutil.trigger_event"](
-                            event_string,
-                            data=event["Actor"]["Attributes"]
-                        )
-                    except Exception:
-                        log.error("An error occurred while forwarding docker event: {}".format(event))
-                else:
-                    log.warning("event['time']: {:} was less than the newest_event_timestamp {:}".format(event['time'], context['newest_event_timestamp']))
-            
-            time.sleep(10)
-
-        # Initialize and run message processor
-        # edmp.init(__salt__, __opts__,
-        #     hooks=settings.get("hooks", []),
-        #     workers=settings.get("workers", []),
-        #     reactors=settings.get("reactors", []))
-        # edmp.run()
+                try:
+                    log.info('attributes raw: {}'.format(event["Actor"]["Attributes"]))
+                    event_string = "system/docker/container/{:}/{:}".format(event["Actor"]["Attributes"]["name"], event["Action"])
+                    data = { k: v for k, v in event["Actor"]["Attributes"].items() }
+                    log.info("Triggering event: {}".format(event))
+                    __salt__["minionutil.trigger_event"](
+                        event_string,
+                        data=data
+                    )
+                except Exception:
+                    log.error("An error occurred while forwarding docker event: {}".format(event))
+            else:
+                log.warning("event['time']: {:} was less than the newest_event_timestamp {:} - Event has likely already been handled.".format(event['time'], context['newest_event_timestamp']))
 
     except Exception:
-        log.exception("Failed to start event reactor")
+        log.exception("Failed to start docker monitor")
 
         raise
     finally:
-        log.info("Stopping event reactor")
+        log.info("Stopping docker monitor")
