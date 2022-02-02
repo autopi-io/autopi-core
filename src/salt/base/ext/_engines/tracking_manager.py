d3a9e419846fb1350964dbbc3c43b6a785c98abe
Eriks Petersons <eriks@autopi.io>
2022-02-02 15:08:11 +0100

    Geofence functionality

--- src/salt/base/ext/_engines/tracking_manager.py
+++ src/salt/base/ext/_engines/tracking_manager.py
@@ -2,7 +2,10 @@ import datetime
 import logging
 import pynmea2
 import salt.loader
+import yaml
 
+from pygeodesy.sphericalNvector import LatLon
+from math import radians, cos, sin, sqrt, atan2
 from messaging import EventDrivenMessageProcessor, extract_error_from
 from salt_more import SuperiorCommandExecutionError
 from serial_conn import SerialConn
@@ -17,6 +20,11 @@ context = {
         "error": None,
         "last_recorded": None,
         "last_reported": None,
+    },
+    "geofence": {
+        "error": None,
+        "repeat_count_to_trigger_change": 3,
+        "fences": []
     }
 }
 
@@ -26,6 +34,8 @@ edmp = EventDrivenMessageProcessor("tracking", context=context, default_hooks={"
 # Serial connection
 conn = SerialConn()
 
+# Geofence object array
+geofence_objects = []
 
 @edmp.register_hook(synchronize=False)
 def status_handler():
@@ -219,6 +229,179 @@ def significant_position_filter(result):
 
     return new_pos
 
+class Geofence:    
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.repeat_count = 0
+        self.last_recorded = None
+        self.state = None
+
+    def is_inside(self, coords):
+        pass
+
+    def reset_count(self):
+        self.repeat_count = 0
+
+    def increment_count(self):
+        self.repeat_count += 1
+
+    def set_state(self, new_state):
+        self.state = new_state
+
+    def set_last_recorded(self, reading):
+        self.last_recorded = reading
+
+class GeofencePolygon(Geofence):
+    # corner_coords: [(lat, lon, seq)]
+    def __init__(self, id, name, corner_coords):
+        Geofence.__init__(self, id, name)
+        
+        def sortKey(point):
+            return point["seq"] 
+        corner_coords.sort(key=sortKey)
+
+        self.corner_coords = [LatLon(corner["lat"], corner["lon"]) for corner in corner_coords]
+        
+    def is_inside(self, coords):
+        locCoord = LatLon(*coords)
+        return locCoord.isenclosedBy(self.corner_coords)
+
+class GeofenceCircle(Geofence):
+    def __init__(self, id, name, center_coords, radius):
+        Geofence.__init__(self, id, name)
+        self.center_coords = (center_coords["lat"], center_coords["lon"])
+        self.radius = radius
+
+    @staticmethod
+    def get_distance(point1, point2):
+        R = 6373.0
+
+        lat1 = radians(point1[0])
+        lon1 = radians(point1[1])
+        lat2 = radians(point2[0])
+        lon2 = radians(point2[1])
+
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distance = R * c
+
+        return distance
+
+    @staticmethod
+    def is_in_circle(vehiclePosition, circleCenter, cirlceRadius):
+        dist = GeofenceCircle.get_distance((vehiclePosition[0], vehiclePosition[1]), (circleCenter[0], circleCenter[1]))
+        return dist <= cirlceRadius
+
+    def is_inside(self, coords):
+        dist = GeofenceCircle.get_distance(coords, self.center_coords)
+        return dist <= self.radius
+
+def update_geofence_context(fence_objects):
+    fence_dict = []
+    for fence in fence_objects:
+        fence_dict.append({
+            "id": fence.id,
+            "name": fence.name,
+            "state": fence.state
+        })
+    context["geofence"]["fences"] = fence_dict
+
+def init_geofence_objects():
+
+    log.info("Initializing geofence objects")
+    
+    try:
+        file = open("/etc/salt/minion.d/geofences.yaml", "r")
+        fences_dict = yaml.load(file)
+        file.close()
+
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("Creating fences from: {}".format(fences_dict))
+       
+        # Create objects for all geofences and store in global array
+        for fence in fences_dict:
+            if fence["shape"] == "SHAPE_POLYGON":
+                geofence_objects.append(
+                    GeofencePolygon(
+                        id=fence["id"],
+                        name=fence["name"],
+                        corner_coords=fence["polygon_corners"]
+                ))
+
+            elif fence["shape"] == "SHAPE_CIRCLE":
+                geofence_objects.append(
+                    GeofenceCircle(
+                        id=fence["id"],
+                        name=fence["name"],
+                        center_coords=fence["circle_center"],
+                        radius=fence["circle_radius"]
+                ))
+
+            else:
+                log.warn("Invalid geofence shape of '{}' for fence '{}'".format(fence["shape"], fence["name"]))
+    
+    except Exception as err:
+        log.warn("Failed while initializing geofence objects: {}".format(err))
+        return
+
+    update_geofence_context(geofence_objects)
+            
+
+@edmp.register_hook(synchronize=False)
+def geofence_event_trigger(result):
+    """
+    Listens for position results and triggers geofence inside/outside events
+    """
+
+    if log.isEnabledFor(logging.DEBUG):
+        log.debug("Checking all geofences")
+
+    if isinstance(result, Exception):
+        log.warn("No data from GPS, can not determine geofence status. Exception: {}".format(result))
+        return
+
+    updated = False
+    ctx = context["geofence"]
+    current_location_coords = (result["loc"]["lat"], result["loc"]["lon"])
+
+    # Check if the device is within a geofence
+    for fence in geofence_objects:
+
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("Checking geofence {} ({})".format(fence.id, fence.name))
+
+        new_state = "outside"
+
+        # Check if vehicle is inside the geofence
+        if fence.is_inside(current_location_coords):
+            new_state = "inside"
+
+        # Update repeat counter
+        if fence.last_recorded == new_state:
+            fence.increment_count()
+        else:
+            fence.reset_count()
+
+        # Check if fence's state needs to be updated and event triggered 
+        if fence.repeat_count >= ctx["repeat_count_to_trigger_change"] and fence.state != new_state:
+            fence.set_state(new_state)
+            updated = True
+
+            edmp.trigger_event(
+                {"reason": ctx["error"], "fence": fence.id} if ctx["error"] else {"fence": fence.id},
+                "vehicle/geofence/{:s}".format(fence.state))
+
+        # prepare fence for next check
+        fence.set_last_recorded(new_state)
+
+    # if change happended, update the context
+    if updated:
+        update_geofence_context(geofence_objects)    
 
 @intercept_exit_signal
 def start(**settings):
@@ -228,6 +411,9 @@ def start(**settings):
 
         # Initialize serial connection
         conn.init(settings["serial_conn"])
+
+        # Initialize geofence objects
+        init_geofence_objects()
 
         # Initialize and run message processor
         edmp.init(__salt__, __opts__,
