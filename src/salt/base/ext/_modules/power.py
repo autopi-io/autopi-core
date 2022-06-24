f04602c97318019a27e120c7d9ff6bfbc2be722f
Henrik Nilsson <henrik@autopi.io>
2022-06-24 13:00:21 +0200

    Fix modem_off query

--- src/salt/base/ext/_modules/power.py
+++ src/salt/base/ext/_modules/power.py
@@ -172,7 +172,7 @@ def sleep(interval=60, delay=60, modem_off=False, acc_off=False, confirm=False, 
     if modem_off:
         if __opts__.get("spm.version", 2.0) >= 3.0:
             try:
-                __salt__["spm.query"]("usr_pins", low="sw_3v3")
+                __salt__["spm.query"]("sys_pins", low="sw_3v3")
             except:
                 log.exception("Failed to power off modem 3V3 supply")
         else:
