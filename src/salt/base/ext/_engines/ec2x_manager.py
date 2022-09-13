279ac3f50ba4449cdeb7316b29872042ec47ae5b
Henrik Nilsson <henrik@autopi.io>
2022-09-13 17:01:15 +0200

    Ensure echo on as default on modems

--- src/salt/base/ext/_engines/ec2x_manager.py
+++ src/salt/base/ext/_engines/ec2x_manager.py
@@ -500,6 +500,7 @@ def start(**settings):
 
         # Initialize serial connection
         conn.init(settings["serial_conn"])
+        conn.on_open = lambda: _exec("ATE1")  # Ensure echo is on
 
         # Initialize and run message processor
         edmp.init(__salt__, __opts__,
